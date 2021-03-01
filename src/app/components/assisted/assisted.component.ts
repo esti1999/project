149b4fa3cb4820a672e8cb36d6442accb2f39bdf
John Doe <johndoe@email.com>
2021-03-01 19:18:59 +0200

    create assisted

---
+++ src/app/components/assisted/assisted.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assisted',
+  templateUrl: './assisted.component.html',
+  styleUrls: ['./assisted.component.css']
+})
+export class AssistedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

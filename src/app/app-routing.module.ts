f3abd1fe65b46e054e60189e2a8022f8e05ba0b1
John Doe <johndoe@email.com>
2021-03-04 15:02:13 +0200

    jjj

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { VolunteerComponent } from './components/volunteer/volunteer.component';
+import { AssistedComponent } from './components/assisted/assisted.component';
+import { VolunteeringDomainComponent } from './components/volunteering-domain/volunteering-domain.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent},
   { path: 'volunteer', component: VolunteerComponent},
-  { path: 'assisted', component: LoginComponent},
-  { path: 'volunteering-domain', component: LoginComponent},
+  { path: 'assisted', component: AssistedComponent},
+  { path: 'volunteering-domain', component: VolunteeringDomainComponent},
 ];
 
 

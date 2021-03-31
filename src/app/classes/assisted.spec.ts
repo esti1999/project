468e07e5386f7e5a28c164742d8bd1ba71ee50bb
ליאל בוקובזה <florinb1972@gmail.com>
2021-03-31 16:55:40 +0300

    31/03

--- src/app/classes/assisted.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Assisted } from './assisted';
-
-describe('Assisted', () => {
-  it('should create an instance', () => {
-    expect(new Assisted()).toBeTruthy();
-  });
-});
--- src/app/classes/volunteering-domain.spec.ts
+++
@@ -1,7 +0,0 @@
-import { VolunteeringDomain } from './volunteering-domain';
-
-describe('VolunteeringDomain', () => {
-  it('should create an instance', () => {
-    expect(new VolunteeringDomain()).toBeTruthy();
-  });
-});

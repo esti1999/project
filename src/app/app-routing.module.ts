ab9a480ac33265973ec0fcb80dbc3bb24e6575f1
John Doe <johndoe@email.com>
2021-04-18 13:13:24 +0300

    18/04

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { LoginComponent } from './components/login/login.component';
 import { VolunteerComponent } from './components/volunteer/volunteer.component';
 import { AssistedComponent } from './components/assisted/assisted.component';
 import { VolunteeringDomainComponent } from './components/volunteering-domain/volunteering-domain.component';
+import { ScheduleComponent } from './components/schedule/schedule.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent},
   { path: 'volunteer', component: VolunteerComponent},
+  { path: 'schedule', component: ScheduleComponent},
   { path: 'assisted', component: AssistedComponent},
   { path: 'volunteering-domain/:id', component: VolunteeringDomainComponent},
   { path: 'assistedVolunteeringDomain', component: AssistedComponent},

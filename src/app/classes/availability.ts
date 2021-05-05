18242e13908612813f30e472cf5133113233478f
John Doe <johndoe@email.com>
2021-05-05 13:14:55 +0300

    05/05

--- src/app/classes/availability.ts
+++ src/app/classes/availability.ts
@@ -1,7 +1,5 @@
 export class Availability{
-<<<<<<< HEAD
-    constructor(public code_availability?:number, public code_day?:number, public code_shift?:number, public IsSelected:boolean=false){}
-=======
+
     constructor(
         public code_availability?:number,
         public code_day?:number,
@@ -10,5 +8,5 @@ export class Availability{
         public shift_description?:string,
         public IsSelected?:boolean
     ){}
->>>>>>> 76b55a1c475092a68083d317a9de65a52617564c
+
 }
